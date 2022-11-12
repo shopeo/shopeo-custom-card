77e11865d70a688edb051129007dbacb9f2bfbd8
jabber <2898117012@qq.com>
2022-11-12 19:35:57 +0800

    init

--- shopeo-custom-card.php
+++ shopeo-custom-card.php
@@ -53,3 +53,15 @@ add_action( 'wp_enqueue_scripts', function () {
 		'ajax_url' => admin_url( 'admin-ajax.php' )
 	) );
 } );
+
+add_action( 'woocommerce_single_product_summary', function () {
+	global $product;
+	if ( $product ) {
+		echo "<a class='button' href='#'>" . __( 'Custom Now', 'shopeo-custom-card' ) . "</a>";
+	}
+}, 40 );
+
+add_action( 'wp', function () {
+	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+} );
